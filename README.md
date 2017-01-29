e12656b54d7916fd169737280ea44544a4760b00
Filipe Dias <f.dias@runtime-revolution.com>
2017-01-29 21:53:16 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-small-stuff
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'chef-small-stuff'
+maintainer 'The Authors'
+maintainer_email 'f.dias@runtime-revolution.com'
+license 'mit'
+description 'Installs/Configures chef-small-stuff'
+long_description 'Installs/Configures chef-small-stuff'
+version '0.1.0'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/chef-small-stuff/issues' if respond_to?(:issues_url)
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/chef-small-stuff' if respond_to?(:source_url)
